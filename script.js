5d292d8bb5761532be78d287a4c508761e61db22
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-05-12 20:53:47 -0300

    Adicionar blocos de resposta

--- script.js
+++ script.js
@@ -2,4 +2,18 @@ console.log('JS carregado');
 
 function validacao() {
     console.log('Iniciando validação de CPF');
+
+    var cpf = document.getElementById('cpf_digitado').value;
+    
+    var resultadoValidacao = validaCPF(cpf);
+
+    if(resultadoValidacao) {
+        document.getElementById('success').style.display = 'block';
+    } else {
+        document.getElementById('error').style.display = 'block';
+    }
+}
+
+function validaCPF(cpf) {
+    return true;
 }
