db9d3f06ac3b5a5731220c8144b11cfad118f63a
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-05-12 21:08:41 -0300

    Validacao primeiro digito

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function validacao() {
     console.log('Iniciando validação de CPF');
 
     var cpf = document.getElementById('cpf_digitado').value;
-    
+
     var resultadoValidacao = validaCPF(cpf);
 
-    if(resultadoValidacao) {
+    if (resultadoValidacao) {
         document.getElementById('success').style.display = 'block';
     } else {
         document.getElementById('error').style.display = 'block';
@@ -16,16 +16,25 @@ function validacao() {
 
 function validaCPF(cpf) {
     console.log(cpf.length);
-    if(cpf.length != 11) {
+    if (cpf.length != 11) {
         return false;
     }
 
-    var numeros = cpf.substring(0,9);
+    var numeros = cpf.substring(0, 9);
     var digitos = cpf.substring(9);
 
     var soma = 0;
-    for(var i  = 10; i > 1; i--) {
+    for (var i = 10; i > 1; i--) {
         soma += numeros.charAt(10 - i) * i;
     }
+
+    var resto = soma % 11;
+    var resultado = resto < 2 ? 0 : resto;
+
+    // Validacao do primeiro digito
+    if (resultado != digitos.charAt(0)) {
+        return false;
+    }
+
     return true;
 }
