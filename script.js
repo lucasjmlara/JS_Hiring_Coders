8b150a9701facc9d3f96f64802e1c7289105b21a
Lucas Lara <lucasjosemartinsdelara@gmail.com>
2022-05-12 21:03:20 -0300

    Inicio validaCPF()

--- script.js
+++ script.js
@@ -15,5 +15,17 @@ function validacao() {
 }
 
 function validaCPF(cpf) {
+    console.log(cpf.length);
+    if(cpf.length != 11) {
+        return false;
+    }
+
+    var numeros = cpf.substring(0,9);
+    var digitos = cpf.substring(9);
+
+    var soma = 0;
+    for(var i  = 10; i > 1; i--) {
+        soma += numeros.charAt(10 - i) * i;
+    }
     return true;
 }
